0a560b5d91472b58bd66393faa5907054031cd01
Pritam <pritamjana360@gmail.com>
2024-03-16 23:23:47 +0530

    V-1.2.7
    add order page

--- src/app/account/checkout/page.tsx
+++ src/app/account/checkout/page.tsx
@@ -4,21 +4,7 @@ import axios from "axios";
 import { cookies } from "next/headers";
 import Styles from "@/app/style/checkoutpage.module.css";
 import { useEffect, useState } from "react";
-interface CartProducts {
-  id: string;
-  productId: string;
-  title: string;
-  imageUrl: string;
-  qty: number;
-  price: number;
-  proTotalPrice: number;
-}
-interface CartData {
-  products: Array<CartProducts>;
-  totalPrice: string;
-  totalLength: number;
-  token: string;
-}
+
 export default function CheckoutPage({
   params,
   searchParams,
@@ -29,135 +15,72 @@ export default function CheckoutPage({
 
   const token = searchParams;
   // const [cartData, setCartData] = useState<CartData | null | undefined>();
-  const { hostUrl, profileData , cartData} = useAppContext();
+  const { hostUrl, profileData, cartData } = useAppContext();
   const [payOption, setPayOption] = useState(0);
   const [addressIndex, setAddressIndex] = useState(0);
 
-  // const orderProduct = async () => {
-
-  //   try {
-
-  //     const response = await fetch(`${hostUrl}/api/order?type=cart`, {
-  //       method: 'post',
-  //       headers: {
-  //         'auth-token': String(searchParams?.id)
-  //       },
-  //       credentials: 'include',
-  //     });
-
-  //     setCartData(await response.json());
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-  // const getCartProduct = async () => {
-  //   try {
-
-  //     const response = await axios.get(`${hostUrl}/api/product/cartproduct`, { withCredentials: true })
-
-  //     setCartData(await response.data)
-  //   } catch (error) {
-  //     console.log(error)
-  //   }
-  // }
-
-  // console.log(searchParams);
-  useEffect(() => {
-    // getPostData()
-    // orderProduct();
-    // getCartProduct()
-  }, [])
-
-  // const generateTransId = (length:number)=>{
-
-  //   const smLetter = "abcdefghijklmnopqrstuvwxyz";
-  //   const cpLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  //   const numericLetter = "123456789";
 
 
 
-  //   return length;
-
-  // }
+  const payNow = async (type: String) => {
 
 
-  function generateTransId(length:number) {
-    const numCount = 4; // Minimum count of numeric characters
-    const smCount = 4; // Minimum count of alphabetic characters
-    const cpCount = 4;
-
-    // Characters to use in the OTP
-    const numericCharacters = '123456789';
-    const cpCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    const smCharacters = 'abcdefghijklmnopqrstuvwxyz';
-
-    let transId = '';
-
-    // Generate minimum required numeric characters
-    for (let i = 0; i < numCount; i++) {
-        transId += numericCharacters.charAt(Math.floor(Math.random() * numericCharacters.length));
-    }
-
-    // Generate minimum required alphabetic characters
-    for (let i = 0; i < smCount; i++) {
-        transId += cpCharacters.charAt(Math.floor(Math.random() * cpCharacters.length));
-    }
-
-       // Generate minimum required alphabetic characters
-       for (let i = 0; i < smCount; i++) {
-        transId += smCharacters.charAt(Math.floor(Math.random() * smCharacters.length));
-    }
-
-    // Generate remaining characters randomly
-    const remainingLength = length - (numCount + cpCount + smCount);
-    const characters = numericCharacters + cpCharacters;
-
-    for (let i = 0; i < remainingLength; i++) {
-        transId += characters.charAt(Math.floor(Math.random() * characters.length));
-    }
-
-    // Shuffle the OTP characters to randomize their positions
-    transId = shuffleString(transId);
-
-    return transId;
-}
-
-// const createOtp = generateOTP(6);
-
-// Function to shuffle the characters in the OTP string
-function shuffleString(str:string) {
-    const arr = str.split('');
-    for (let i = arr.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [arr[i], arr[j]] = [arr[j], arr[i]];
-    }
-    return arr.join('');
-}
-
-  const payNow = ()=>{
-
-    const address  =profileData?.address[addressIndex];
+    const address = profileData?.address[addressIndex];
     const item = cartData?.products
     const totalPrice = cartData?.totalPrice;
 
-    const transId = generateTransId(30);
+    // const transId = generateTransId(30);
 
-    const Data = JSON.stringify({address,item, totalPrice, transId});
+    const Data = JSON.stringify({ address, item, totalPrice});
 
+    if (type === "cod") {
 
+      try {
 
-    console.log(Data);
+        const response = await fetch(`${hostUrl}/api/order?type=cart&mode=cod`, {
+          method:"POST",
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body:Data
+        });
 
+        console.log(await response.json())
+      } catch (error) {
 
+        console.log(error);
+      }
+    }
+    else {
+      try {
+
+        const response = await fetch(`${hostUrl}/api/order?type=cart&mode=paynow`, {
+          method:'POST',
+          credentials:'include',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body:Data
+        });
+
+        console.log(await response.json())
+      } catch (error) {
+        console.log(error);
+      }
+
+      console.log(Data, type);
+    }
   }
+
+
 
   return (
     <div className={Styles.page}>
       {profileData && profileData.address.map((item, index) => {
         return (
-          <div key={index} className={`${Styles.addressContainer} ${addressIndex === index ? Styles.activeAddressContainer : Styles.addressContainer}`} onClick={()=> setAddressIndex(index)}>
-            <input type="radio" name="addressCheckBox" id={item?.id} className={Styles.inputRadio}  checked={addressIndex===index} onChange={()=> setAddressIndex(index)} />
+          <div key={index} className={`${Styles.addressContainer} ${addressIndex === index ? Styles.activeAddressContainer : Styles.addressContainer}`} onClick={() => setAddressIndex(index)}>
+            <input type="radio" name="addressCheckBox" id={item?.id} className={Styles.inputRadio} checked={addressIndex === index} onChange={() => setAddressIndex(index)} />
             <div>
 
               <p>{item?.addressline}</p>
@@ -194,7 +117,7 @@ function shuffleString(str:string) {
             <div className={Styles.payOption}><input className={Styles.inputRadio} type="radio" name="payoption" id="" defaultChecked={payOption === 1 ? true : false} onChange={() => setPayOption(1)} /> <p>Pay now</p></div>
           </div>
           <div className={Styles.payBtn}>
-            {payOption === 0 ? <button>Submit</button> : <button onClick={ payNow}>Pay Now ₹{cartData?.totalPrice}</button>}
+            {payOption === 0 ? <button onClick={() => payNow("cod")}>Submit</button> : <button onClick={() => payNow("paynow")}>Pay Now ₹{cartData?.totalPrice}</button>}
           </div>
         </>
         }
---
+++ src/app/account/order/page.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import { useAppContext } from "@/app/context/appContext"
+import { useEffect, useState } from "react";
+import Styles from '@/app/style/orderpage.module.css';
+
+interface productItem {
+    title: string,
+    imageUrl: string,
+    qty: string,
+    price: string,
+    totalPrice: string,
+}
+
+interface orderObject {
+    id: string,
+    user: string,
+    mode: string,
+    product: productItem,
+    address: string,
+    phone: string,
+    transId: string,
+    date: string,
+
+}
+interface OrderData {
+    orderData: Array<orderObject>,
+}
+
+
+export default function orderPage() {
+    const { hostUrl } = useAppContext();
+
+    const [orderData, setOrderData] = useState<OrderData | null | undefined>();
+    const fetchOrderData = async () => {
+        const response = await fetch(`${hostUrl}/api/order`, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        });
+
+        const data = await response.json();
+
+        setOrderData(data);
+
+        // console.log(data);
+    }
+
+
+
+    useEffect(() => {
+        fetchOrderData();
+    }, []);
+
+    return (
+        <div className={Styles.page}>
+
+            <div className={Styles.orderComponent}>
+                {orderData?.orderData.map(item => {
+                    return (
+                        <div key={item?.id} className={Styles.card}>
+
+                           
+                                <img src={item?.product?.imageUrl} alt="" width={150} height={150} />
+                                    <p>{item?.product?.title}</p>
+                                <div>
+                                    {/* <p>{item?.product?.qty}</p>
+                                    <p>{item?.product?.price}</p>
+                                    <p>{item?.product?.totalPrice}</p>
+                                    <p >{item?.address}</p>
+                                    <p>{item?.mode}</p>
+                                    <p>{item?.transId}</p> */}
+                                </div>
+                         
+                        </div>
+                    )
+                })}
+            </div>
+        </div>
+
+    )
+}
