f9d4f5a4a4f5aadbf4f70a9a3472b6a96b8e715b
Pritam <pritamjana360@gmail.com>
2024-04-24 22:55:58 +0530

    V-1.4.5
    Add home other component page

--- components/homeother.tsx
+++ components/homeother.tsx
@@ -1,7 +1,6 @@
 import Styles from '@/app/style/homeheader.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
-import { GrPrevious, GrNext } from "react-icons/gr";
 import FlexOfferCarousel from './FlexOfferCarousel';
 export default function Homeother() {
 
@@ -121,19 +120,19 @@ export default function Homeother() {
                         <h3 className={Styles.titleText}>Up to 75% off | Headphones</h3>
                         <div className={Styles.smallCard}>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Up to 75% off | boAt</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boult.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boult.jpg" alt="" loading='lazy' />
                                 <p>Up to 75% off | boult</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./noise.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/noise.jpg" alt="" loading='lazy' />
                                 <p>Up to 75% off | noise</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./zebronics.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/zebronics.jpg" alt="" loading='lazy' />
                                 <p>Up to 75% off | zebronics</p>
                             </div>
 
@@ -147,19 +146,19 @@ export default function Homeother() {
                         <h3 className={Styles.titleText}>Up to 60% off | Styles for men</h3>
                         <div className={Styles.smallCard}>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Clothing</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Footwear</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Watches</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Bags & Wallets</p>
                             </div>
 
@@ -173,19 +172,19 @@ export default function Homeother() {
                         <h3 className={Styles.titleText}>Starting $99 | All your home improvement needs</h3>
                         <div className={Styles.smallCard}>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Spin mops, wipes & more</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Bathroom hardware & accessories</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Hammers, screwdrivers & more</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Extension boards, plugs & more</p>
                             </div>
 
@@ -199,19 +198,19 @@ export default function Homeother() {
                         <h3 className={Styles.titleText}>Appliances for your home | up to 55% off</h3>
                         <div className={Styles.smallCard}>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Air conditioners</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Refrigerators</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Microwaves</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Washing machines</p>
                             </div>
 
@@ -225,19 +224,19 @@ export default function Homeother() {
                         <h3 className={Styles.titleText}>Revamp your home in style</h3>
                         <div className={Styles.smallCard}>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Bedsheets, curtains & more</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Home decoration</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Home storage</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Lighting solution</p>
                             </div>
 
@@ -251,19 +250,19 @@ export default function Homeother() {
                         <h3 className={Styles.titleText}>Get the perfact screen size | TVs Starting $6,999</h3>
                         <div className={Styles.smallCard}>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Budget TVs, Up to 60% off</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>4K TVs | Up to 24 months No cose EMI</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Big Screen | Free installation</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Ultra Premium TVs | Up to 50% off</p>
                             </div>
 
@@ -277,19 +276,19 @@ export default function Homeother() {
                         <h3 className={Styles.titleText}>Automotive essentials | Up to 60% off</h3>
                         <div className={Styles.smallCard}>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Cleaning accessories</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Tyre & rim care</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Helmets</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Vacume cleaner</p>
                             </div>
 
@@ -303,19 +302,19 @@ export default function Homeother() {
                         <h3 className={Styles.titleText}>Up to 60% off | Styles for women</h3>
                         <div className={Styles.smallCard}>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Wommen clothing</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Footware + Handbags</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Watches</p>
                             </div>
                             <div className={Styles.smallCardItem}>
-                                <img src="./boat.jpg" alt="" loading='lazy' />
+                                <Image width={150} height={200} src="/boat.jpg" alt="" loading='lazy' />
                                 <p>Fasion jewellery</p>
                             </div>
                         </div>
